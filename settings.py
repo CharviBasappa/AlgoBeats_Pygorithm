8a9966e3742fc65e2d8abd842d1d1c8b00f8ad22
Charvi Basappa <charvibasappa98@gmail.com>
2025-04-01 22:12:27 -0700

    Added screen and color settings logic in here

---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 600
+BAR_COLOR = (100, 200, 255)
+BG_COLOR = (0, 0, 0)
+
+pygame.init()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sorting Visualizer")
